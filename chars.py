f440286a7c12fd3a593ff70d05b4cb9d41e83168
qatt12 <40256448+qatt12@users.noreply.github.com>
2018-06-25 12:16:44 -0400

    Updated file:
    Added Functions and refined, ** Still needs work**

--- chars.py
+++ chars.py
@@ -1,35 +1,80 @@
-# Comments are for ideas
-# and explination if needed
-# most things are self explanitory
+# Notes:
+# 	Comments are for ideas and explination if needed
+# 	Most things are self explanitory
+# 	To-Do at bottom
+#  Most print statment are for error checking
+
+import random
+import sys
 
 class enemy(object):
 	def __init__(self):
-		self.hp = 5
+		self.hp = 10
 		self.is_dead = False
+		self.atk_hp = self.atk()
+		self.defe_hp = self.defe()
+#		self.check = print("World")
+	def atk(self):
+		x = random.randint(1,3)
+		return x
+		
+	def defe(self):
+		y = random.randint(1,2)
+		return y
 
-		# for attking make another function
-		# return dmg output
-		# set to 0 and then make random from 0-5
-		# befor attacking
-		# random number 0 would be a miss
-		self.atk = 1
-		# self.def = 1
+
 		# damage taken = atk - def
 
 class player(object):
 	def __init__(self):
 		self.is_dead = False
 		self.hp = 20
-		# set to 0 and then make random from 0-5
-		# random number 0 would be a miss
-		# for attking make another function
-		# return dmg output
-		self.atk = 2 
-		# self.def = 1
-		# damage taken = atk - def
+		self.defe_hp = self.defe()
+		self.atk_hp = self.atk()
+#		self.check = print("Hello") 	# check
+	def atk(self):
+		x = random.randint(0,3)
+		return x
+		
+	def defe(self):
+		y = random.randint(0,2)
 
-# in main can do something like 
-#  badG[enemy() for i in range(0,//random number))]
-# 	to set the number of enemys
-#  the something like 
-#	hero = player()
+
+if __name__ == "__main__":
+	Hero = player()
+	BadE = enemy()
+#	print("FooBar")	# check	
+	while(True):
+		print("--- NEW RUN ---")
+		a = Hero.atk_hp
+		b = BadE.defe_hp
+		print("Hero ATK -> " , a)
+		print("BadE DEF -> " , b)
+		print("BadE HP -> " , BadE.hp)
+		print("Hero HP -> " , Hero.hp)
+
+#	while(True):
+		if(Hero.hp <= 0):
+			print("BadE wins??")
+			break
+		if(BadE.hp <= 0):
+			print("Hero wins!!")
+			break
+		print("BadE_HP -> " , BadE.hp)
+		print("Hero ATK -> " , a)
+		print("BadE DEF -> " , b)
+		BadE.hp = a - b		# this does bad math?
+		print("BadE_HP -> " , BadE.hp)
+	sys.exit()
+
+# In main: 
+#  -> BadE[enemy() for i in range(0,//random number))] to set the number of enemys
+#  -> Fix the random infinity loop
+#  -> Get the atk and def to rerandom every loop
+#  
+# In General:
+#	-> Add menu to make for interactive
+#	-> Refine and make visually better
+#
+#
+#
