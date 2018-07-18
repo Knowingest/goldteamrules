fbcd1074d6f4b11445a9a7f69d9759d3e1ed7dd7
knowingest <harrisonmeyerhill@gmail.com>
2018-07-18 14:40:09 -0400

    making encounter class more generic and usuable.  attempting to set standards for how other components function

--- encounter.py
+++ encounter.py
@@ -8,30 +8,32 @@
 
 
 
-class baddie(): #stand in enemy class
+class baddie(): #stand-in enemy class
 	def __init__(self):
+		self.dmg = 0
 		self.hp = 5
 		self.is_dead = False
+
+	def take_turn(self):
+		self.dmg = 1
 
 class player(): #stand in player class
 	def __init__(self, hp):
 		self.hp = hp
+		self.atk1 = 3
 
 class encounter(): #basically, all combat will occur inside an instance of this class
 
-				#we input current player hp and how many enemies we want
-	def __init__(self, player_hp, enemy_number):
-		self.enemy_team = list()
-		self.p1 = player(player_hp)
-		for x in range(0, enemy_number):
-			self.enemy_team.append(baddie())
-
+				#we input current player hp and a list of enemies
+	def __init__(self, player, enemy_list):
+		self.enemy_team = enemy_list
+		self.p1 = player
 
 				#prints board information to console
 	def print_board(self):
 		print("********")
 		for i in range(0, len(self.enemy_team)):
-			if self.enemy_team[i].is_dead == True:
+			if self.enemy_team[i].hp < 1:
 				print("XX", end = " ")
 			else:
 				print("E" + str(i + 1), end = " ")
@@ -40,22 +42,37 @@ class encounter(): #basically, all combat will occur inside an instance of this 
 		print("********")
 
 		for i in range(0, len(self.enemy_team)):
-			if (self.enemy_team[i].is_dead == False):
+			if (self.enemy_team[i].hp > 0):
 				print("E" + str(i + 1) + " HP = " + str(self.enemy_team[i].hp))
 
 		print("Player HP = " + str(self.p1.hp))
 
+	def take_turn(self, move, target):
+		if move == 1:
+			self.enemy_team[target].hp -= self.p1.atk1
+
+		for e in self.enemy_team:
+			if e.hp > 0:
+				e.take_turn()
+				self.p1.hp -= e.dmg
+
+
+
+
 if __name__ == "__main__":
-	test = encounter(20, 4)
+	b = list()
+	for x in range(5):
+		b.append(baddie())
+	p = player(20)
+	test = encounter(p, b)
 	#for dude in test.enemy_team:
 	#	print(dude.hp)
 
 
 		#print board, kill a dude, print board again
 	test.print_board()
-	print()
-	test.enemy_team[1].is_dead = True
-	test.p1.hp = 13
+	test.take_turn(1, 2)
 	test.print_board()
-
+	test.take_turn(1, 2)
+	test.print_board()
 
