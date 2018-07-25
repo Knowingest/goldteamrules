3920ab6a8588de8d64f41725a5a7b2c6ee81b1fb
Abi <ajanpal@gmail.com>
2018-07-25 15:17:25 -0400

    Add files via upload
    More updates to the GUI

--- window.py
+++ window.py
@@ -20,13 +20,14 @@ class Window(QtWidgets.QWidget):
 class Painting(QtWidgets.QWidget):
 	def __init__(self, parent):
 		QtWidgets.QWidget.__init__(self,parent)
+		self.bg = "First"
 		self.setFixedSize(888,684)
 		self.setup()
 		
 	def setup(self):
-		self.butt = QtWidgets.QPushButton("Draw a background",self)
+		self.butt = QtWidgets.QPushButton("Start Game",self)
 		self.butt.move(20,20)
-		self.butt.clicked.connect(self.arenaArea)
+		self.butt.clicked.connect(self.firstArea)
 		self.back=False
 		self.show()
 	
@@ -58,24 +59,36 @@ class Painting(QtWidgets.QWidget):
 		self.update()
 	
 	def firstArea(self,event):
-		
-	
+		self.secbutt = QtWidgets.QPushButton("Forward",self)
+		self.secbutt.move(20,20)
+		self.secbutt.clicked.connect(self.arenaArea)
+		self.bg = "first"
+		self.show()
+		self.update()
+
 	def secondArea(self,event):
+		self.firstbutt = QtWidgets.QPushButton("Back",self)
+		self.firstbutt.move(20,20)
+		self.secbutt.clicked.connect(self.firstArea)
 		
+		self.thirdbutt = QtWidgets.QPushButton("Forward",self)
+		self.thirdbutt.move(20,20)
+		self.thirdbutt.clicked.connect(self.thirdArea)
+		self.bg = "second"
+		self.show()
+		self.update()
 	
 	def thirdArea(self,event):
-		
+		self.bg = "third"
 	
 	def fourthArea(self,event):
-	
+		self.bg = "fourth"
 	
 	def fifthArea(self,event):
-	
+		self.bg = "fifth"
 	
 	def sixthArea(self,event):
-	
-	
-	def seventhArea(self,event):
+		self.bg = "sixth"
 	
 	
 		
