9dc4fa4e840fb4775225a9216b33f5870af4e065
dl16c <35575217+dl16c@users.noreply.github.com>
2018-07-26 00:43:44 -0400

    d and q
    small fix on test.py

--- test.py
+++ test.py
@@ -1,40 +1,37 @@
 from enemy import *
 
-_blub = blub()
+blub = blub()
 bblub = bigblub()
-_spooder = spooder()
-_snek = snek()
-skull = blub()
+spooder = spooder()
+snek = snek()
+skull = skullitor()
 mother = mother()
-
 team = [blub, bblub, spooder, snek, skull, mother]
-# for x in team:
-#     x.take_turn()
-#     print(x.name)
-#     print(x.msg)
-#     print(x.dmg)
-#     print()
-#
-# for x in team:
-#     x.take_turn()
-#     print(x.name)
-#     print(x.msg)
-#     print(x.dmg)
-#     print()
-#
-# for x in team:
-#     x.take_turn()
-#     print(x.name)
-#     print(x.msg)
-#     print(x.dmg)
-#     print()
-#
-# for x in team:
-#     x.take_turn()
-#     print(x.name)
-#     print(x.msg)
-#     print(x.dmg)
-#     print()
+
+for x in team:
+    x.take_turn()
+    print(x.name)
+    print(x.msg)
+    print(x.dmg)
+    print()
+for x in team:
+    x.take_turn()
+    print(x.name)
+    print(x.msg)
+    print(x.dmg)
+    print()
+for x in team:
+    x.take_turn()
+    print(x.name)
+    print(x.msg)
+    print(x.dmg)
+    print()
+for x in team:
+    x.take_turn()
+    print(x.name)
+    print(x.msg)
+    print(x.dmg)
+    print()
 
 skull.take_turn()
 print(skull.msg)
