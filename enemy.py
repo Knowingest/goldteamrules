32ab7a9430996daa3d17587e79fc1e79035ef8f2
dl16c <35575217+dl16c@users.noreply.github.com>
2018-07-25 23:32:14 -0400

    dan and q
    enemy taketurn funcs

--- enemy.py
+++ enemy.py
@@ -1,56 +1,93 @@
 import random
 
-class enemy(object):
-    def __init__(self, _hp=10, _atk_hp=random.randint(1, 3), _def_hp=random.randint(1, 2)):
-        self.hp = _hp
-        self.atk_hp = _atk_hp
-        self.def_hp = _def_hp
-
-
 class blub(object):
     def __init__(self):
         self.name = "Blub"
-        # attr = enemy(hp, atk, def)
-        self.attr = enemy(random.randint(1,3), random.randint(1,2), random.randint(0,2))
-        self.quote = "Woof"
-        self.attack1 = "Blub uses squish!"
-        self.attack2 = "Blub throws a gelatinous glob!"
+        self.hp = random.randint(3,4)
+        self.dmg = random.randint(1,2)
+        self.msg = "Blub uses squish!"
+
+    def take_turn(self):
+        if self.hp < 3:
+            x = random.randint(1, 2)
+            if x == 1:
+                self.hp += 1
+                self.msg = "Blub is healing!"
 
 class bigblub(object):
     def __init__(self):
         self.name = "Big Blub"
-        self.attr = enemy(random.randint(7, 9), random.randint(1, 2), random.randint(2, 3))
-        self.quote = "Woof"
-        self.attack1 = self.name + " uses squish!"
+        self.hp = random.randint(1, 3)
+        self.dmg = random.randint(1, 2)
+        self.msg = "Blub uses squish!"
+
+    def take_turn(self):
+        if self.hp < 5:
+            x = random.randint(1, 2)
+            if x == 1:
+                self.hp += 2
+                self.msg = "Big Blub is healing!"
+
 
 class spooder(object):
     def __init__(self):
         nameList = ["Spooder", "Spidey", "Spider"]
         self.name = random.choice(nameList)
-        self.attr = enemy(random.randint(5, 8), random.randint(4, 5), random.randint(1, 2))
-        self.quote = "Woof"
-        self.attack1 = self.name + " uses poison!"
+        self.hp = random.randint(1, 3)
+        self.dmg = random.randint(1, 2)
+        self.msg = self.name + " uses poison!"
+
+    def take_turn(self):
+        x = random.randint(1, 3)
+        if x == 1:
+            self.msg = "You have been poisoned!"
+            self.dmg += 1
+        else:
+            self.msg = self.name + " bites you!"
 
 class snek(object):
     def __init__(self):
         nameList = ["Snek", "Snake", "Danger Noodle"]
         self.name = random.choice(nameList)
-        self.attr = enemy(random.randint(3, 5), random.randint(2, 4), random.randint(2, 3))
-        self.quote = "Woof"
-        self.attack1 = self.name + " bites you!"
+        self.hp = random.randint(1, 3)
+        self.dmg = random.randint(1, 2)
+        self.msg = self.name + " bites you!"
+
+    def take_turn(self):
+        x = random.randint(1, 3)
+        if x == 1:
+            self.msg = "You have been poisoned!"
+            self.dmg += 2
+        else:
+            self.msg = self.name + " bites you!"
 
 class skullitor(object):
     def __init__(self):
         nameList = ["Skullitor", "Creepy Skull", "Skull"]
         self.name = random.choice(nameList)
-        self.attr = enemy(random.randint(6, 9), random.randint(5, 8), random.randint(1, 3))
-        self.quote = "Woof"
-        self.attack1 = self.name + " casts a fire spell at you!"
+        self.hp = random.randint(1, 3)
+        self.dmg = 0
+        self.msg = self.name + " casts a fire spell at you!"
+
+        self.counter = 0
+
+    def take_turn(self):
+        counter += 1
+        if (counter == 3):
+            self.dmg = random.randint(8, 12)
+            self.msg = self.name + " casts a charged fire spell at you!"
+        else:
+            self.dmg = 0
+            self.msg = self.name + " is charging an attack!"
+            counter = 0
 
 class mother(object):
     def __init__(self):
         self.name = "Mother"
-        self.attr = enemy(random.randint(13, 15), random.randint(2, 4), random.randint(6, 9))
-        self.quote = "Woof"
-        self.attack1 = "Mother beats you with a belt!"
-        self.attack2 = "Mother looks disappointingly you!"
+        self.hp = random.randint(1, 3)
+        self.dmg = random.randint(1, 2)
+        self.msg = "Mother beats you with a belt!"
+
+    def take_turn(self):
+        if self.hp < 7:
+            self.dmg *= 2
