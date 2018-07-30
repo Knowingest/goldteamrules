4b71bcdccc63cc9d5d4e7c809599f5373398dbff
RicardoAndresDC <31515033+RicardoAndresDC@users.noreply.github.com>
2018-07-30 15:12:28 -0400

    Imports

--- drake.py
+++ drake.py
@@ -1,4 +1,11 @@
 '''new file'''
 
+import importlib
+import sys
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+importlib.import_module("window")
+importlib.import_module("encounter")
+
 print("GOOD LUCK")
 
