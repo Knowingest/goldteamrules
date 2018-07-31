4059fb4b8367c33ad5f89e88abc475ebb00422bf
RicardoAndresDC <31515033+RicardoAndresDC@users.noreply.github.com>
2018-07-31 11:56:51 -0400

    \_(o.o)_/

--- window.py
+++ window.py
@@ -37,6 +37,25 @@ class Painting(QtWidgets.QWidget):
 		#self.grid.addWidget(self.butt,1,1)
 		#self.lay.setLayout(self.grid)
 	
+	def hideyobutts(self):
+		if self.bg =="first":
+			self.
+		elif self.bg=="second":
+			bush.setTextureImage(QtGui.QImage("background2.png"))
+		elif self.bg=="third":
+			bush.setTextureImage(QtGui.QImage("background3.png"))
+		elif self.bg=="fourth":
+			bush.setTextureImage(QtGui.QImage("background4.png"))
+		elif self.bg=="fifth":
+			bush.setTextureImage(QtGui.QImage("background5.png"))
+		elif self.bg=="sixth":
+			bush.setTextureImage(QtGui.QImage("background6.png"))
+		else: #self.bg=="arena":
+			bush.setTextureImage(QtGui.QImage("bigarena.png"))
+
+
+
+
 	def paintEvent(self, event):
 		
 		qp = QtGui.QPainter()
@@ -72,7 +91,7 @@ class Painting(QtWidgets.QWidget):
 		self.update()
 	
 	def firstArea(self,event):
-		self.butt.setParent(None)
+		self.butt.hide()
 		self.bg = "first"
 		self.update()
 		self.secbutt = QtWidgets.QPushButton("Forward",self)
@@ -83,6 +102,7 @@ class Painting(QtWidgets.QWidget):
 		self.secbutt.show()
 
 	def secondArea(self,event):
+		self.butt.setParent(None)
 		self.firstbutt = QtWidgets.QPushButton("Back",self)
 		self.firstbutt.move(20,20)
 		self.secbutt.clicked.connect(self.firstArea)
@@ -91,11 +111,14 @@ class Painting(QtWidgets.QWidget):
 		self.thirdbutt.move(20,20)
 		self.thirdbutt.clicked.connect(self.thirdArea)
 		
+		self.firstbutt.show()
+		self.thirdbutt.show()
 		self.bg = "second"
 		self.show()
 		self.update()
 	
 	def thirdArea(self,event):
+		self.
 		self.secbutt = QtWidgets.QPushButton("Back", self)
 		self.secbutt.move(0,0)
 		self.secbutt.clicked.connect(self.secondArea)
@@ -108,6 +131,9 @@ class Painting(QtWidgets.QWidget):
 		self.fivbutt.move(35,35)
 		self.fivbutt.clicked.connect(self.fifthArea)
 
+		self.secbutt.show()
+		self.fobutt.show()
+		self.fivbutt.show()
 		self.bg = "third"
 		self.show()
 		self.update()
@@ -117,6 +143,7 @@ class Painting(QtWidgets.QWidget):
 		self.thirdbutt.move(0,0)
 		self.thirdbutt.clicked.connect(self.thirdArea)
 
+		self.thirdbutt.show()
 		self.bg = "fourth"
 		self.show()
 		self.update()
@@ -130,6 +157,8 @@ class Painting(QtWidgets.QWidget):
 		self.sexbutt.move(0,0)
 		self.sexbutt.clicked.connect(self.sixthArea)
 
+		self.thirdbutt.show()
+		self.sexbutt.show()
 		self.bg = "fifth"
 		self.show()
 		self.update()
@@ -139,6 +168,7 @@ class Painting(QtWidgets.QWidget):
 		self.fivbutt.move(0,0)
 		self.fivbutt.clicked.connect(self.fifthArea)
 
+		self.fivbutt.show()
 		self.bg = "sixth"
 		self.show()
 		self.update()
