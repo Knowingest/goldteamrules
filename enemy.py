e214d92e7e414cf453dc502c862f6426acabab2c
dl16c <35575217+dl16c@users.noreply.github.com>
2018-08-01 19:37:13 -0400

    more
    more

--- enemy.py
+++ enemy.py
@@ -4,7 +4,7 @@ import random
 class blub(object):
     def __init__(self):
         self.name = "Blub"
-        self.hp = random.randint(3,4)
+        self.hp = random.randint(2,3)
         self.dmg = random.randint(1,2)
         self.msg = "Blub uses squish!"
 
@@ -13,13 +13,14 @@ class blub(object):
             x = random.randint(1, 2)
             if x == 1:
                 self.hp += 1
-                self.msg = "Blub is healing!"
+                self.msg = "Blub is healing!\nBlub heals for " + x + " health."
+        print(self.msg)
 
 class bigblub(object):
     def __init__(self):
         self.name = "Big Blub"
-        self.hp = random.randint(1, 3)
-        self.dmg = random.randint(1, 2)
+        self.hp = random.randint(2, 4)
+        self.dmg = random.randint(4, 5)
         self.msg = "Blub uses squish!"
 
     def take_turn(self):
@@ -28,14 +29,15 @@ class bigblub(object):
             if x == 1:
                 self.hp += 2
                 self.msg = "Big Blub is healing!"
+        print(self.msg)
 
 
 class spooder(object):
     def __init__(self):
         nameList = ["Spooder", "Spidey", "Spider"]
         self.name = random.choice(nameList)
-        self.hp = random.randint(1, 3)
-        self.dmg = random.randint(1, 2)
+        self.hp = random.randint(3, 5)
+        self.dmg = random.randint(2, 3)
         self.msg = self.name + " uses poison!"
 
     def take_turn(self):
@@ -45,13 +47,14 @@ class spooder(object):
             self.dmg += 1
         else:
             self.msg = self.name + " bites you!"
+        print(self.msg)
 
 class snek(object):
     def __init__(self):
         nameList = ["Snek", "Snake", "Danger Noodle"]
         self.name = random.choice(nameList)
-        self.hp = random.randint(1, 3)
-        self.dmg = random.randint(1, 2)
+        self.hp = random.randint(2, 3)
+        self.dmg = random.randint(3, 4)
         self.msg = self.name + " bites you!"
 
     def take_turn(self):
@@ -61,12 +64,13 @@ class snek(object):
             self.dmg += 2
         else:
             self.msg = self.name + " bites you!"
+        print(self.msg)
 
 class skullitor(object):
     def __init__(self):
         nameList = ["Skullitor", "Creepy Skull", "Skull"]
         self.name = random.choice(nameList)
-        self.hp = random.randint(1, 3)
+        self.hp = random.randint(3, 5)
         self.dmg = 0
         self.msg = self.name + " casts a fire spell at you!"
 
@@ -77,17 +81,20 @@ class skullitor(object):
         if (counter == 3):
             self.dmg = random.randint(8, 12)
             self.msg = self.name + " casts a charged fire spell at you!"
+            print(self.msg)
         else:
             self.dmg = 0
             self.msg = self.name + " is charging an attack!"
+            print(self.msg)
             counter = 0
 
 class mother(object):
     def __init__(self):
         self.name = "Mother"
-        self.hp = random.randint(1, 3)
-        self.dmg = random.randint(1, 2)
+        self.hp = random.randint(5, 6)
+        self.dmg = random.randint(4, 5)
         self.msg = "Mother beats you with a belt!"
+        print(self.msg)
 
     def take_turn(self):
         if self.hp < 7:
